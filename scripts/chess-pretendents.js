5c90c62f7053bb18f6d6d6df406569a88382b876
Allegro75 <otkidach75@yandex.ru>
2020-01-21 00:49:04 +0300

    Started chess-pretendents file

---
+++ scripts/chess-pretendents.js
@@ -0,0 +1,18 @@
+//
+//Для ВСПЛЫТИЯ окон с подробной статистикой по клику на ячейку большой таблицы:
+//
+
+var table = document.querySelector('table.main-table');
+table.onclick = function() {
+    var target = event.target;
+    while (target != table) {
+        if (target.getAttribute('class') == 'statistics has-history') {
+            var tableDataId = target.getAttribute('id');
+            var urlInWindowOpen = '../chess/chess-small-tables/' +
+            tableDataId + '.html';
+            window.open(urlInWindowOpen, '', 'width=650px, height=900px');
+            return;
+            }
+        target = target.parentNode;
+        }
+    };
