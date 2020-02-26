ef61eceeed6f2ed370aa521d6657602a04c356a7
Allegro75 <otkidach75@yandex.ru>
2020-02-26 07:11:08 +0300

    Made history12 & history24 files

---
+++ scripts/history12.js
@@ -0,0 +1,40 @@
+//
+//Пробую делать отдельный файл со скриптами для history
+//
+
+
+//
+//Для ВСПЛЫТИЯ окон с подробной статистикой по клику на ячейку большой таблицы:
+//
+
+var table = document.querySelector('table.main-table');
+table.onclick = function() {
+    var target = event.target;
+    while (target != table) {
+        if (target.getAttribute('class') == 'statistics has-history') {
+            var tableDataId = target.getAttribute('id');
+            var urlInWindowOpen = 'football-small-tables/' +
+            tableDataId + '.html';
+            window.open(urlInWindowOpen, '', 'width=650px, height=900px');
+            return;
+            }
+        target = target.parentNode;
+        }
+    };
+
+
+    
+// Переключатель в ДУЭЛЬНыЙ вид:
+let results = document.querySelectorAll('.results');
+let duels = document.querySelectorAll('.duels');
+const duelsSwitch = document.querySelector('.duel-button button.results-duels-switch');
+
+duelsSwitch.onclick = function () {showHideDuels();};
+const showHideDuels = () => {
+    for (i = 0; i < results.length; i++) {
+        results[i].hidden = !results[i].hidden;
+        duels[i].hidden = !duels[i].hidden; 
+    }
+};
+
+
