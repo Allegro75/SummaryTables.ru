83e7fca85d9e7b185d298d0882b325d344aa8ba4
Allegro75 <otkidach75@yandex.ru>
2020-02-28 02:29:53 +0300

    Updated with EuroLeague games (+ MC in hist36)

---
+++ scripts/history36.js
@@ -0,0 +1,19 @@
+
+//
+//Для ВСПЛЫТИЯ окон с подробной статистикой по клику на ячейку большой таблицы:
+//
+
+var table = document.querySelector('table.main-table');
+table.onclick = function() {
+    var target = event.target;
+    while (target != table) {
+        if (target.getAttribute('class') == 'statistics has-history') {
+            var tableDataId = target.getAttribute('id');
+            var urlInWindowOpen = 'football-small-tables/' +
+            tableDataId + '.html';
+            window.open(urlInWindowOpen, '', 'width=650px, height=900px');
+            return;
+            }
+        target = target.parentNode;
+        }
+    };
