0f3564ddd725ccf3af6f8aac029fc8f096a4db4d
Allegro75 <otkidach75@yandex.ru>
2020-02-29 08:27:34 +0300

    Added Fiorentina to history36

--- scripts/history36.js
+++ scripts/history36.js
@@ -17,3 +17,17 @@ table.onclick = function() {
         target = target.parentNode;
         }
     };
+
+ 
+//let addTDInnerHTML = ''; 
+//let addTD = document.createElement('td'); 
+
+// for (let i = 1; i < table.rows.length - 1; i += 1) {
+//     addTDInnerHTML = table.rows[i].cells[25].innerHTML;
+//     //addTD.innerHTML = addTDInnerHTML;
+//     table.rows[i].append(document.createElement('td'));
+//     table.rows[i].cells[31].innerHTML = addTDInnerHTML;
+//     table.rows[i].cells[31].id = table.rows[i].cells[25].getAttribute('id');
+// };   
+
+//table.rows[1].append(table.rows[1].cells[25]);    
