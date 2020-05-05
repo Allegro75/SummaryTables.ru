ddb721301a4384e7a90d47abdcc0f2ae41b85856
Allegro75 <otkidach75@yandex.ru>
2020-05-05 20:36:40 +0300

    Added CUP WINNERS cup to HREFS in small tables

--- scripts/small-tables/add_hrefs.js
+++ scripts/small-tables/add_hrefs.js
@@ -8,7 +8,7 @@ const addHref = (node, year, tourney) => {
     node.innerHTML = `
                             <a href="http://wildstat.ru/p/50/ch/EUR_${tourney[0]}_${year - 1}_${year}" 
                             target="_blank"
-                            title="Посмотреть турнир ${tourney[1]} чемпионов ${year - 1}/${year}">
+                            title="Посмотреть турнир ${tourney[1]} ${year - 1}/${year}">
                                 ${node.innerHTML}
                             </a>                            
                         `;
@@ -27,6 +27,8 @@ const defineYearForGroup2Stage = (ri) => {
 };
 
 
+// Кубок/лига чемпионов:
+
 for (let i = 1; i < table.rows.length; i ++) {
     if ( (table.rows[i].cells[4]) &&
     (table.rows[i].cells[4].textContent.includes(`чемпионов`)) ) {
@@ -36,18 +38,18 @@ for (let i = 1; i < table.rows.length; i ++) {
             if (table.rows[i].cells[5].textContent === `группа` ||
             table.rows[i].cells[5].textContent === `отбор`) {
                 for (let ci = 2; ci <= 5; ci ++) {
-                    addHref(table.rows[i].cells[ci], +table.rows[i].cells[3].textContent + 1, ['CL', 'лиги']);
+                    addHref(table.rows[i].cells[ci], +table.rows[i].cells[3].textContent + 1, ['CL', 'лиги чемпионов']);
                 }
             }
             else if (table.rows[i].cells[5].textContent === `группа2`) {
                 let year = defineYearForGroup2Stage(i);
                 for (let ci = 2; ci <= 5; ci ++) {
-                    addHref(table.rows[i].cells[ci], year, ['CL', 'лиги']);
+                    addHref(table.rows[i].cells[ci], year, ['CL', 'лиги чемпионов']);
                 }                
             }
             else {
                 for (let ci = 2; ci <= 5; ci ++) {
-                    addHref(table.rows[i].cells[ci], +table.rows[i].cells[3].textContent, ['CL', 'лиги']);
+                    addHref(table.rows[i].cells[ci], +table.rows[i].cells[3].textContent, ['CL', 'лиги чемпионов']);
                 } 
             }
         }
@@ -57,7 +59,7 @@ for (let i = 1; i < table.rows.length; i ++) {
         (+table.rows[i].cells[3].textContent < 1995) ) {
             
             let tour = (table.rows[i].cells[4].textContent.includes(`Лига`)) ?
-            ['CL', 'лиги'] : ['CC', 'кубка'];            
+            ['CL', 'лиги чемпионов'] : ['CC', 'кубка чемпионов'];            
             if (table.rows[i].cells[5].textContent === `группа`) {
                 let year = defineYearForGroup2Stage(i);
                 for (let ci = 2; ci <= 5; ci ++) {
@@ -88,15 +90,49 @@ for (let i = 1; i < table.rows.length; i ++) {
             (table.rows[i].cells[5].textContent === `1/2`) ||
             (table.rows[i].cells[5].textContent === `1/4`) ) {
                 for (let ci = 2; ci <= 5; ci ++) {
-                    addHref(table.rows[i].cells[ci], +table.rows[i].cells[3].textContent, ['CC', 'кубка']);
+                    addHref(table.rows[i].cells[ci], +table.rows[i].cells[3].textContent, ['CC', 'кубка чемпионов']);
                 }                 
             }
             else {
                 for (let ci = 2; ci <= 5; ci ++) {
-                    addHref(table.rows[i].cells[ci], +table.rows[i].cells[3].textContent + 1, ['CC', 'кубка']);
+                    addHref(table.rows[i].cells[ci], 
+                    +table.rows[i].cells[3].textContent + 1, ['CC', 'кубка чемпионов']);
                 }                
             }
         }        
     }
 }
+
+// Кубок кубков:
     
+for (let i = 1; i < table.rows.length; i ++) {
+    if ( (table.rows[i].cells[4]) &&
+    (table.rows[i].cells[4].textContent.includes(`кубков`)) ) {
+
+        if (+table.rows[i].cells[3].textContent >= 1961) {
+
+            if ( (table.rows[i].cells[5].textContent === `ФИНАЛ`) ||
+            (table.rows[i].cells[5].textContent === `1/2`) ||
+            (table.rows[i].cells[5].textContent === `1/4`) ) {
+                for (let ci = 2; ci <= 5; ci ++) {
+                    addHref(table.rows[i].cells[ci], +table.rows[i].cells[3].textContent, ['CWC', 'кубка кубков']);
+                }                 
+            }
+            else {
+                for (let ci = 2; ci <= 5; ci ++) {
+                    addHref(table.rows[i].cells[ci], 
+                    +table.rows[i].cells[3].textContent + 1, ['CWC', 'кубка кубков']);
+                }                
+            }
+        }
+
+        // Специально для первого кубка кубков (1960/1961), в к-ром
+        // 1/4 финала игралась в 1960 г.:        
+        else {
+            for (let ci = 2; ci <= 5; ci ++) {
+                addHref(table.rows[i].cells[ci], 
+                +table.rows[i].cells[3].textContent + 1, ['CWC', 'кубка кубков']);
+            }
+        }
+    }
+}
