5d2eb42ed8aee3027adcfb2df3ed1b24a278ac92
Allegro75 <otkidach75@yandex.ru>
2020-07-04 21:35:01 +0300

    Made script to HIGHLIGHT rows

---
+++ scripts/highlightRows.js
@@ -0,0 +1,46 @@
+
+// Для ПОДСВЕЧИВАНИЯ ячеек:
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const allCurrentRows = document.querySelectorAll(`tr.current`);
+
+    const allLegendStripes = document.querySelectorAll(`.table-explanation__explanation`);
+
+    const highlightRows = (parameter, color) => {
+        const rowsToHighlight = document.querySelectorAll(`tr.${parameter}`);
+        rowsToHighlight.forEach(item => {item.style.backgroundColor = color;});
+    }
+
+    const returnColor = parameter => {
+        const rowsToReturn = document.querySelectorAll(`tr.${parameter}`);
+        rowsToReturn.forEach(item => {item.style.backgroundColor = ``;});
+    }
+
+    allLegendStripes.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            if ( item.classList.contains(`explanation_russia`) ) {
+                item.style.backgroundColor = `#FF9999`;
+                highlightRows(`russia`, `#FF9999`);
+            } else if ( item.classList.contains(`explanation_ukraine`) ) {
+                item.style.backgroundColor = `#9999ff`;
+                highlightRows(`ukraine`, `#9999ff`);
+            } else if ( item.classList.contains(`explanation_current`) ) {
+                item.style.backgroundColor = `#ffffcc`;
+                highlightRows(`current`, `#ffffcc`);
+            }
+        });
+        item.addEventListener('mouseout', () => {
+            if ( item.classList.contains(`explanation_russia`) ) {
+                returnColor(`russia`);
+                item.style.backgroundColor = ``;
+            } else if ( item.classList.contains(`explanation_ukraine`) ) {
+                returnColor(`ukraine`);
+                item.style.backgroundColor = ``;
+            } else if ( item.classList.contains(`explanation_current`) ) {
+                returnColor(`current`);
+                item.style.backgroundColor = ``;
+            }
+        });
+    });
+}); 
