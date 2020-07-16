2061b2605fcef85fcac9c11e1710610a09029c59
Allegro75 <otkidach75@yandex.ru>
2020-07-16 17:34:18 +0300

    made SHOW small table func for alphabet

---
+++ scripts/showSmallTableOnAbRowClick.js
@@ -0,0 +1,13 @@
+
+// Для ВСПЛЫТИЯ окон с подробной статистикой по клику на строку из списка:
+
+document.addEventListener(`DOMContentLoaded`, () => {
+    const allStatRows = document.querySelectorAll(`.ab-row`);
+
+    allStatRows.forEach( item => item.addEventListener('click', () => {
+        let tableDataId = item.getAttribute(`id`);
+        let urlInWindowOpen = `football-small-tables/${tableDataId}.html`;
+        window.open(urlInWindowOpen, ``, `width=520px, height=970px, top=0, left=0`);
+    }) );
+})
+
