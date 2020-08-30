556b33ab63c357b920345315a815a8b41943bd3c
Allegro75 <otkidach75@yandex.ru>
2020-08-31 00:44:59 +0300

    Added CupWinnersCup + mini-corrections

---
+++ eurocups/cwc/CC.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="ru" class="football">
+
+<?php
+// Получаем массив МАТЧЕЙ турнира:
+require_once '../../config.php';
+require_once '../../connect.php';
+$conn = connect();
+$sql = "SELECT * FROM `eurocups_matches` WHERE tourneyTitle = 'Кубок кубков' AND tourneyFinalYear = {$_GET['year']}";
+$result = mysqli_query($conn, $sql);
+$matches = array();
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $matches[] = $row;
+    }
+}
+// echo '<pre>';
+// print_r($matches[47]);
+// var_dump($matches[47]['hadToss'] == 1);
+// echo '</pre>';
+
+// Получаем массив КЛУБОВ-участников турнира:
+$clubNames = [];
+for ($i = 0; $i < count($matches); $i++) {
+    $clubNames[] = $matches[$i]["firstClubName"];
+    $clubNames[] = $matches[$i]["secondClubName"];
+}
+$clubNames = array_unique($clubNames);
+$clubs = [];
+foreach ($clubNames as $value) {
+    $sql = "SELECT * FROM `eurocups_clubs` WHERE basicFullName = '{$value}' OR altNames = '{$value}' OR altNames LIKE '{$value},%' OR altNames LIKE '%, {$value}' OR altNames LIKE '%, {$value},%'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $clubs[] = $row;
+}
+// echo '<pre>';
+// print_r(count($clubs));
+
+$tourStartYear = $_GET['year'] - 1;
+$tourFinalYear = $_GET['year'];
+
+// Печатаем head:
+echo 
+"<head>
+    <meta charset='utf-8'>
+    <meta name='author' content='Edwards, Allegro, Edwards75, Allegro75, Oleg Otkidach'>
+    <meta name='author' content='Олег Откидач'>
+    <meta name='description' content='Кубок обладателей кубков {$tourStartYear}/{$tourFinalYear}'>
+    <meta name='keywords' content='Кубок кубков. {$tourStartYear}. {$tourFinalYear}.
+    Футбол. Еврокубки. Европа. 
+    Статистика. История. Результаты.    
+    Лига чемпионов. Кубок чемпионов. Лига Европы. Кубок УЕФА. Суперкубок.'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <link rel='shortcut icon' href='../../images/football_ball.svg' type='image/x-icon'>
+    <title>Кубок кубков {$tourStartYear}/{$tourFinalYear}</title>
+    <link rel='stylesheet' href='../../stylesheets/football__body.css'>
+    <link rel='stylesheet' href='../../stylesheets/cap-wo-nav.css'>
+    <link rel='stylesheet' href='../../stylesheets/navigation.css'>
+    <link rel='stylesheet' href='../../stylesheets/captions.css'>
+    <link rel='stylesheet' href='../../stylesheets/tourneys.css'>
+    <link rel='stylesheet' href='../../stylesheets/donate.css'>
+    <link rel='stylesheet' href='../../stylesheets/footer.css'>
+</head>
+
+<body class='football__body'>
+    <div class='football__background'>";
+
+// Печатаем шапку:
+require_once('templates/header.php');    
+?>
+
+        <main>
+
+            <!-- Заголовок -->
+            <section class="captions">
+                <h1 class="captions__h1">
+                    КУБОК обладателей КУБКОВ
+                        <?php
+                        echo "{$tourStartYear}/{$tourFinalYear}";
+                        ?>
+                </h1>
+            </section>
+
+            <!-- Турнир -->
+            <div class="tourney">
+
+                <?php
+
+                require_once('functions.php');
+
+            // Разбираемся с ФИНАЛОМ:
+                // Определяем финальный матч(и):                
+                $finalMatches = getStageMatches($matches, 'Финал');
+                // echo '<pre>';
+                // print_r($finalMatches);
+
+                // Определяем коды участников финального матча:
+                $club1Name = $finalMatches[0]["firstClubName"];
+                $club2Name = $finalMatches[0]["secondClubName"];
+                $club1 = getClubByName($club1Name, $clubs);
+                $club2 = getClubByName($club2Name, $clubs);
+                $code1 = getImageAdress($club1, $imagesList);
+                $code2 = getImageAdress($club2, $imagesList);
+
+                // Определяем счёт финального матча: 
+                $firstGoals = $finalMatches[0]["firstClubGoals"];
+                $secondGoals = $finalMatches[0]["secondClubGoals"];
+
+                // Определяем место финального матча: 
+                $finCity = $finalMatches[0]["fieldCity"];
+                $finCountry = $finalMatches[0]["fieldCountry"];
+
+                // Определяем дату финального матча: 
+                $finDate = $finalMatches[0]["date"];
+                $finYear = $finalMatches[0]["year"];
+
+                // Разбираемся с доп. временем и ПЕНАЛЬТИ:
+                $add = '';
+                $penalty = '';
+                $lastMatchIndex = count($finalMatches) - 1;
+                if ($finalMatches[$lastMatchIndex]["hadEfficientAddTime"]) {
+                    if ($finalMatches[$lastMatchIndex]['hadPenalties']) {
+                        $penalty = "<div class='penalty'>(доп. время, победа по пенальти)</div>";   
+                    }
+                    else {
+                        $penalty = "<div class='penalty'>(доп. время)</div>";
+                    }
+                } else if ($finalMatches[$lastMatchIndex]['hadPenalties']) {
+                    $penalty = "<div class='penalty'>(победа по пенальти)</div>";
+                }
+                if ($finalMatches[$lastMatchIndex]["comments"] === 'доп. матч') {
+                    $penalty2 = "<div class='penalty'>(доп. матч)</div>"; 
+                }
+
+                // Начинаем упорядочивать массив с клубами:
+                $orderedClubs = [];
+                $orderedClubs[0] = $club1;
+                $orderedClubs[1] = $club2;
+
+                // Печатаем ФИНАЛ:
+                echo
+                    "<div class='tourney__stage tour-stage stage_final'>
+
+                    <div class='tour-stage_name'>ФИНАЛ</div>
+
+                    <div class='tour-stage__content stage-content'>
+
+                        <div class='stage-content__row content-row content-row_row-1'>
+
+                            <div class='club-info club-info_1'>
+                                <div class='logo logo-left'>
+                                    <img src='../../images/{$code1[0]}' alt='{$club1Name}' class='{$code1[1]}' title='{$code1[2]}'>
+                                </div>
+                                <div class='club-name club-name_winner'>
+                                    {$club1Name}
+                                </div>
+                            </div>
+
+                            <div class='score'>
+                                {$firstGoals} : {$secondGoals}
+                            </div>
+
+                            <div class='club-info club-info_2'>
+                                <div class='club-name'>
+                                    {$club2Name}
+                                </div> 
+                                <div class='logo logo-right'>
+                                    <img src='../../images/{$code2[0]}' alt='{$club2Name}' class='{$code2[1]}' title='{$code2[2]}'>
+                                </div>  
+                            </div>  
+
+                        </div>
+
+                        <div class='stage-content__row content-row content-row_row-2'>
+                            {$penalty}
+                            <div class='field'>
+                                {$finCity}, {$finCountry}
+                            </div>    
+                            <div class='date'>
+                                {$finDate}.{$finYear}
+                            </div>                                            
+                        </div>";
+
+
+                    //Если матчей в ФИНАЛЕ ДВА:
+                    if (count($finalMatches) === 2) {
+                    // Определяем счёт ВТОРОГО финального матча: 
+                    $firstGoals = $finalMatches[1]["firstClubGoals"];
+                    $secondGoals = $finalMatches[1]["secondClubGoals"];
+
+                    // Определяем место ВТОРОГО финального матча: 
+                    $finCity = $finalMatches[1]["fieldCity"];
+                    $finCountry = $finalMatches[1]["fieldCountry"];
+
+                    // Определяем дату ВТОРОГО финального матча: 
+                    $finDate = $finalMatches[1]["date"];
+                    $finYear = $finalMatches[1]["year"];
+                    echo 
+                    "<div class='stage-content__row content-row content-row_row-1'>
+
+                        <div class='club-info club-info_1'>
+                        </div>
+
+                        <div class='score'>
+                            {$firstGoals} : {$secondGoals}
+                        </div>
+
+                        <div class='club-info club-info_2'>  
+                        </div>  
+
+                    </div>
+
+                    <div class='stage-content__row content-row content-row_row-2'>
+                        {$penalty2}
+                        <div class='field'>
+                            {$finCity}, {$finCountry}
+                        </div>    
+                        <div class='date'>
+                            {$finDate}.{$finYear}
+                        </div>                                            
+                    </div>";                    
+                    }
+
+                echo
+                    "</div>
+
+                </div>";
+
+                // Пишем следующую стадию и дополняем массив клубов:
+                $orderedClubs = writeMatchesByStage('1/2 финала', $matches, $orderedClubs, $clubs, $imagesList);
+
+                // Пишем следующую стадию и дополняем массив клубов:
+                $orderedClubs = writeMatchesByStage('1/4 финала', $matches, $orderedClubs, $clubs, $imagesList);
+
+                // Пишем следующую стадию и дополняем массив клубов:
+                if ( getStageMatches($matches, '1/8 финала') ) {
+                    $orderedClubs = writeMatchesByStage('1/8 финала', $matches, $orderedClubs, $clubs, $imagesList);
+                }
+
+                // Пишем следующую стадию и дополняем массив клубов:
+                if ( getStageMatches($matches, '1/16 финала') ) {
+                    $orderedClubs = writeMatchesByStage('1/16 финала', $matches, $orderedClubs, $clubs, $imagesList);
+                }                
+
+                // Пишем следующую стадию и дополняем массив клубов:
+                if ( getStageMatches($matches, 'Предварительный раунд') ) {
+                    $orderedClubs = writeMatchesByStage('Предварительный раунд', $matches, $orderedClubs, $clubs, $imagesList);
+                }                
+                // echo '<pre>';
+                // print_r($orderedClubs);
+
+                ?>
+
+        </main>
+
+<?php
+require_once('templates/donate.php');
+require_once('templates/footer.php');
+?>
+
+    </div>
+
+</body>
+
+</html>
---
+++ eurocups/cwc/templates/header.php
@@ -0,0 +1,253 @@
+        
+        <!-- Шапка -->
+        <header class="cap">
+
+            <div class="cap__site-title-stripe site-title-stripe">
+
+                <a href="../../index.html" title="Фавориты Лиги чемпионов 2019/2020. Истории встреч. Сводная таблица.">
+                    <img class="site-title-stripe__icon  site-title-stripe__icon_left" src="../../images/football_ball_yellow.svg" alt="футбол">
+                </a>
+
+                <a href="../../index.html" title="Фавориты Лиги чемпионов 2019/2020. Истории встреч. Сводная таблица.">
+                    <img class="site-title-stripe__icon  site-title-stripe__icon_right" src="../../images/football_ball_yellow.svg" alt="футбол">
+                </a>
+
+                <div class="site-title-stripe__text">
+                    <a href="../../index.html" title="Фавориты Лиги чемпионов 2019/2020. Истории встреч. Сводная таблица.">
+                        Футбол <span class="site-title-stripe__text_delimiter">&#8226;</span>
+                        Суперклубы <span class="site-title-stripe__text_delimiter">&#8226;</span>
+                        Истории личных встреч <span class="site-title-stripe__text_delimiter">&#8226;</span>
+                        Сводные таблицы
+                    </a>
+                </div>
+
+            </div>
+
+            <nav class="nav">
+                <div class="nav__stripe football">
+                    <!-- <div class="nav__stripe__title">
+                     <a href="../../history36.html" title="Лучшие клубы Европы. Личные встречи. Сводная таблица">
+                         ФУТБОЛ:
+                     </a>
+                     </div> -->
+                    <ul class="nav__stripe__list">
+                        <li class="nav__stripe__list__item  dropdown  current-season">
+                            <a href="../../index.html" title="Фавориты еврокубков 2019/2020. Личные встречи. Сводная таблица">
+                                <div class="nav__icon current">
+                                    <!-- <img src="../../images/screamer.png" alt="текущие турниры"
+                              class="nav__icon current nav__icon_yellow">
+                           <img src="../../images/screamer_black.png" alt="текущие турниры"
+                              class="nav__icon current nav__icon_black"> -->
+                                </div>
+                                ТЕКУЩИЙ СЕЗОН &#8729; Сводная таблица
+                            </a>
+                            <ul class="nav__stripe__list__item__droplist">
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../index.html" title="Фавориты Лиги чемпионов 2019/2020">
+                                        Лига чемпионов 2019/2020
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../euroleague.html" title="Фавориты Лиги Европы 2019/2020">
+                                        Лига Европы 2019/2020
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav__stripe__list__item  dropdown  ussr">
+                            <a href="../../russia.html" title="Российские (и советские) клубы против европейских. Личные встречи. Сводная таблица">
+                                <div class="nav__icon ussr">
+                                    <!-- <img src="../../images/ussr.png" alt="Россия/СССР" class="nav__icon ussr nav__icon_yellow">
+                           <img src="../../images/ussr_black.png" alt="Россия/СССР"
+                              class="nav__icon ussr nav__icon_black"> -->
+                                </div>
+                                РОССИЯ / СССР
+                            </a>
+                            <ul class="nav__stripe__list__item__droplist">
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../russia.html" title="Российские клубы против европейских">
+                                        Россия
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../ukraine.html" title="Украинские клубы против европейских">
+                                        Украина
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../byelorussia.html" title="Белорусские клубы против европейских">
+                                        Белоруссия
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../kazakhstan.html" title="Казахстанские клубы против европейских. Личные встречи. Сводная таблица">
+                                        Казахстан
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav__stripe__list__item  dropdown  history">
+                            <a href="../../history36.html" title="Лучшие клубы Европы за всю историю. Личные встречи. Сводная таблица">
+                                <div class="nav__icon history">
+                                    <!-- <img src="./images/history_icon.png" alt="история"
+                              class="nav__icon history nav__icon_yellow">
+                           <img src="./images/history_icon_black.png" alt="история"
+                              class="nav__icon history nav__icon_black"> -->
+                                </div>
+                                вся ИСТОРИЯ
+                            </a>
+                            <ul class="nav__stripe__list__item__droplist">
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../history.html" title="12 лучших клубов Европы за всю историю">
+                                        <span class="figures-in-history-hrefs">12</span> клубов
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../history24.html" title="24 лучших клуба Европы за всю историю"> <span class="figures-in-history-hrefs">24</span> клуба </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../history36.html" title="36 лучших клубов Европы за всю историю"> <span class="figures-in-history-hrefs">36</span> клубов </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../winners.html" title="Все 22 победителя Лиги/кубка чемпионов всех лет">
+                                        Победители ЛЧ
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../range.html" title="Список 100 лучших клубов Европы">
+                                        Ранжир
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="nav__stripe__list__item  decade">
+                            <a href="../../decade.html" title="24 лучших клуба десятилетия (2011-2020). Личные встречи. Сводная таблица">
+                                <div class="nav__icon decade">
+                                    <!-- <img src="./images/ten.png" alt="десятилетие" class="nav__icon decade nav__icon_yellow">
+                           <img src="./images/ten_black.png" alt="десятилетие"
+                              class="nav__icon decade nav__icon_black"> -->
+                                </div>
+                                Десятилетие
+                            </a>
+                        </li>
+
+                        <li class="nav__stripe__list__item  range">
+                            <a href="../../range.html" title="Список 100 лучших клубов в истории">
+                                <div class="nav__icon range">
+                                    <!-- Сюда через css подставляется иконка -->
+                                </div>
+                                Ранжир
+                            </a>
+                        </li>
+
+                        <li class="nav__stripe__list__item dropdown alphabet">
+
+                            <a href="../../alphabet.html" title="Алфавитный список лучших клубов и их соперников">
+                                <div class="nav__icon alphabet">
+                                    <!-- Сюда через css подставляется иконка -->
+                                </div>
+                                АЛФАВИТ
+                            </a>
+
+                            <ul class="nav__stripe__list__item__droplist">
+
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_01.html" title="К букве 'А'">
+                                        А
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_02.html" title="К буквам 'А', 'Б'">
+                                        А Б
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_03.html" title="К букве 'Б'">
+                                        Б
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_04.html" title="К буквам 'В', 'Г'">
+                                        В Г
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_05.html" title="К буквам 'Г', 'Д', 'И'">
+                                        Г Д И
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_06.html" title="К буквам 'К', 'Л'">
+                                        К Л
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_07.html" title="К букве 'М'">
+                                        М
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_08.html" title="К буквам 'М', 'О', 'П'">
+                                        М О П
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_09.html" title="К буквам 'П', 'Р'">
+                                        П Р
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_10.html" title="К буквам 'Р', 'С'">
+                                        Р С
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_11.html" title="К буквам 'С', 'Т'">
+                                        С Т
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_12.html" title="К буквам 'Ф', 'Ц'">
+                                        Ф Ц
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../alphabet/alphabet_13.html" title="К буквам 'Ч', 'Ю'">
+                                        Ч Ю
+                                    </a>
+                                </li>
+
+                            </ul>
+
+                        </li>
+
+                        <li class="nav__stripe__list__item  dropdown  chess">
+                            <a href="../../chess/pretendents_2020.html" title="Шахматы" target="_blank">
+                                <div class="nav__icon chess">
+                                    <!-- <img src="./images/chess/white_pawn_13px.png" alt="шахматы"
+                              class="nav__icon chess nav__icon_yellow">
+                           <img src="./images/chess/black_pawn_13px.png" alt="шахматы"
+                              class="nav__icon chess nav__icon_black"> -->
+                                </div>
+                                Шахматы
+                            </a>
+                            <ul class="nav__stripe__list__item__droplist">
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../chess/pretendents_2020.html" title="Участники турнира претендентов 2020. Личные встречи. Сводная таблица" target="_blank">Претенденты 2020. Личные счета</a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../https://www.youtube.com/channel/UCCNvNNsRKNqvsJK9rd4PpMQ" title="Шахматный канал Олега Откидача (aka Edwards).
+Лучшие партии автора, прежде всего в шахматы Фишера, игранные заочно. 
+Играем в позиционные шахматы.
+Только качественные партии - никакого блица!" target="_blank">
+                                        Youtube-канал о шахматах
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </header>
