c2feb744c10b61c83e6e237b782979a068b8ed6b
Allegro75 <otkidach75@yandex.ru>
2020-11-20 21:37:10 +0300

    Added Zur to change-logo-brightness script

--- scripts/archive/zenitLogo.js
+++ scripts/archive/zenitLogo.js
@@ -5,26 +5,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const queryPart = `table img.football-logo-table[src='../../images/`;
     const logosToLightInTables = document.querySelectorAll(`${queryPart}Zen.png'], ${queryPart}Mar.png'], ${queryPart}Nan.png']`);
 
-    // Если таких логотипов нет, то уходим:
-    if (logosToLightInTables.length === 0) return;
+    const logoCode = (elem) => elem.getAttribute(`src`).slice(-7, -4);
 
-    // Если же есть:
-    else {
-        const logoCode = (elem) => elem.getAttribute(`src`).slice(-7, -4);     
+    // Если такие логотипы есть:
+    if (logosToLightInTables.length > 0) {
 
         // Запускаем перебор логотипов:
         for (let i = 0; i <= logosToLightInTables.length - 3; i += 3) {
-            
+
             // Определяем таблицу, в к-рой находится наше лого:
             const zenitTable = logosToLightInTables[i].parentElement.parentElement.parentElement.parentElement;
 
             // Определяем номер группы:
             const groupNumber = +zenitTable.querySelector(`thead td:first-child`).innerText;
-            // Если номер группы нечётный, то уходим:
-            if ((groupNumber % 2) === 1) return;
 
-            // Если же чётный:
-            else {
+            // Если номер группы чётный:
+            if ((groupNumber % 2) === 0) {
                 // Меняем лого на светлый в верхней строке:
                 logosToLightInTables[i].setAttribute('src', `../../images/${logoCode(logosToLightInTables[i])}_light.png`);
 
@@ -41,4 +37,24 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
     }
+
+    // Ищем логотипы для ЗАМЕНЫ на ТЁМНОЕ в групповых таблицах:
+    const logosToDarkInTables = document.querySelectorAll(`${queryPart}Zur.png']`);
+
+    // Если такие логотипы есть:
+    if (logosToDarkInTables.length > 0) {
+
+        // Запускаем перебор логотипов:
+        for (let i = 0; i <= logosToDarkInTables.length - 3; i += 3) {
+
+            // Определяем строку нашего клуба:
+            const zurichRowNumber = +logosToDarkInTables[i + 1].parentElement.previousElementSibling.innerText;
+            // Если номер строки нечётный:
+            if ((zurichRowNumber % 2) === 1) {
+                logosToDarkInTables[i + 1].setAttribute('src', `../../images/${logoCode(logosToDarkInTables[i + 1])}_dark.png`);
+                logosToDarkInTables[i + 2].setAttribute('src', `../../images/${logoCode(logosToDarkInTables[i + 2])}_dark.png`);
+            }
+        }
+    }
+    
 });
