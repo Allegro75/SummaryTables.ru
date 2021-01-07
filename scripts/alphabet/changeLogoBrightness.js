e92765e6617e22dfa11effa69884cc9afb4981aa
Allegro75 <otkidach75@yandex.ru>
2021-01-08 02:45:03 +0300

    Corr

--- scripts/alphabet/changeLogoBrightness.js
+++ scripts/alphabet/changeLogoBrightness.js
@@ -27,21 +27,22 @@ document.addEventListener('DOMContentLoaded', () => {
         queryPart = `.ab-row img[src='images/`;
         pathToImage = `images/`;
         allLogosToLight = document.querySelectorAll(`${queryPart}And.png'], ${queryPart}Mar.png']`);       
-        if (allLogosToLight.length === 0) return;
     }
 
     // Перебираем массив логотипов для ОСВЕТЛЕНИЯ:
-    allLogosToLight.forEach(elem => {
-        // Находим список, в к-ром находится данный логотип:
-        let listWLogo = elem.parentElement.parentElement.parentElement.parentElement;
-        // Находим строку, в к-рой находится данный логотип:
-        let strWLogo = elem.parentElement.parentElement.parentElement;        
-        // Находим индекс строки с данным логотипом в списке:
-        let indexInList = [...listWLogo.children].indexOf(strWLogo);
-        if ((indexInList % 2) === 0) {
-           elem.setAttribute('src', `${pathToImage}${logoCode(elem)}_light.png`);
-        }            
-    });
+    if (allLogosToLight.length > 0) {
+        allLogosToLight.forEach(elem => {
+            // Находим список, в к-ром находится данный логотип:
+            let listWLogo = elem.parentElement.parentElement.parentElement.parentElement;
+            // Находим строку, в к-рой находится данный логотип:
+            let strWLogo = elem.parentElement.parentElement.parentElement;        
+            // Находим индекс строки с данным логотипом в списке:
+            let indexInList = [...listWLogo.children].indexOf(strWLogo);
+            if ((indexInList % 2) === 0) {
+               elem.setAttribute('src', `${pathToImage}${logoCode(elem)}_light.png`);
+            }            
+        });
+    }
 
     if (tryToMakeDarkList.length > 0 ) {
         allLogosToDark = tryToMakeDarkList;
@@ -52,20 +53,21 @@ document.addEventListener('DOMContentLoaded', () => {
         queryPart = `.ab-row img[src='images/`;
         pathToImage = `images/`;
         allLogosToDark = document.querySelectorAll(`${queryPart}And.png']`);;       
-        if (allLogosToDark.length === 0) return;
     }
 
     // Перебираем массив логотипов для ЗАТЕМНЕНИЯ:
-    allLogosToDark.forEach(elem => {
-        // Находим список, в к-ром находится данный логотип:
-        let listWLogo = elem.parentElement.parentElement.parentElement.parentElement;
-        // Находим строку, в к-рой находится данный логотип:
-        let strWLogo = elem.parentElement.parentElement.parentElement;        
-        // Находим индекс строки с данным логотипом в списке:
-        let indexInList = [...listWLogo.children].indexOf(strWLogo);
-        if ((indexInList % 2) === 1) {
-           elem.setAttribute('src', `${pathToImage}${logoCode(elem)}_dark.png`);
-        }            
-    });    
+    if (allLogosToDark.length > 0) {
+        allLogosToDark.forEach(elem => {
+            // Находим список, в к-ром находится данный логотип:
+            let listWLogo = elem.parentElement.parentElement.parentElement.parentElement;
+            // Находим строку, в к-рой находится данный логотип:
+            let strWLogo = elem.parentElement.parentElement.parentElement;        
+            // Находим индекс строки с данным логотипом в списке:
+            let indexInList = [...listWLogo.children].indexOf(strWLogo);
+            if ((indexInList % 2) === 1) {
+               elem.setAttribute('src', `${pathToImage}${logoCode(elem)}_dark.png`);
+            }            
+        });  
+    }  
 
 });
---
+++ scripts/archive/changeLogoBrightness.js
@@ -0,0 +1,97 @@
+// Для ЗАМЕНЫ логотипов на лого другой светлоты:
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Ищем логотипы для ЗАМЕНЫ на СВЕТЛОЕ в групповых таблицах:
+    const queryPart = `table img.football-logo-table[src='../../images/`;
+    const logosToLightInTables = document.querySelectorAll(`${queryPart}Zen.png'], ${queryPart}Mar.png'], ${queryPart}Nan.png'], ${queryPart}And_dark.png'], ${queryPart}DyK.png']`);
+
+    const logoCode = (elem) => {
+        if (elem.getAttribute(`src`).slice(-9, -4) === `_dark`) {
+            return elem.getAttribute(`src`).slice(-12, -9);
+        } else
+        return elem.getAttribute(`src`).slice(-7, -4);
+    }
+
+    // Если такие логотипы есть:
+    if (logosToLightInTables.length > 0) {
+
+        // Запускаем перебор логотипов:
+        for (let i = 0; i <= logosToLightInTables.length - 3; i += 3) {
+
+            // Определяем таблицу, в к-рой находится наше лого:
+            const zenitTable = logosToLightInTables[i].parentElement.parentElement.parentElement.parentElement;
+
+            // Определяем номер группы:
+            const groupNumber = +zenitTable.querySelector(`thead td:first-child`).innerText;
+
+            // Если номер группы чётный:
+            if ((groupNumber % 2) === 0) {
+                // Меняем лого на светлый в верхней строке:
+                logosToLightInTables[i].setAttribute('src', `../../images/${logoCode(logosToLightInTables[i])}_light.png`);
+
+                // Определяем строку нашего клуба:
+                const zenitRowNumber = +logosToLightInTables[i + 1].parentElement.previousElementSibling.innerText;
+                // Если номер строки нечётный, то уходим:
+                if ((zenitRowNumber % 2) === 1) continue;
+                // Если же чётный:
+                else {
+                    logosToLightInTables[i + 1].setAttribute('src', `../../images/${logoCode(logosToLightInTables[i + 1])}_light.png`);
+                    logosToLightInTables[i + 2].setAttribute('src', `../../images/${logoCode(logosToLightInTables[i + 2])}_light.png`);
+                }
+            }
+        }
+    }
+
+    // Ищем логотипы для ЗАМЕНЫ на ТЁМНОЕ в групповых таблицах:
+    const logosToDarkInTables = document.querySelectorAll(`${queryPart}Zur.png']`);
+
+    // Если такие логотипы есть:
+    if (logosToDarkInTables.length > 0) {
+
+        // Запускаем перебор логотипов:
+        for (let i = 0; i <= logosToDarkInTables.length - 3; i += 3) {
+
+            // Определяем строку нашего клуба:
+            const zurichRowNumber = +logosToDarkInTables[i + 1].parentElement.previousElementSibling.innerText;
+            // Если номер строки нечётный:
+            if ((zurichRowNumber % 2) === 1) {
+                logosToDarkInTables[i + 1].setAttribute('src', `../../images/${logoCode(logosToDarkInTables[i + 1])}_dark.png`);
+                logosToDarkInTables[i + 2].setAttribute('src', `../../images/${logoCode(logosToDarkInTables[i + 2])}_dark.png`);
+            }
+        }
+    }
+
+    // Ищем логотипы для ЗАМЕНЫ на ТЁМНОЕ на СВЕТЛО-СЕРОМ в групповых таблицах:
+    const logosOnLightGrey = document.querySelectorAll(`${queryPart}DyK.png']`);
+
+    // Если такие логотипы есть:
+    if (logosOnLightGrey.length > 0) {
+
+        // Запускаем перебор логотипов:
+        for (let i = 0; i <= logosOnLightGrey.length - 3; i += 3) {
+
+            // Определяем таблицу, в к-рой находится наше лого:
+            const logoTable = logosOnLightGrey[i].parentElement.parentElement.parentElement.parentElement;
+
+            // Определяем номер группы:
+            const groupNumber = +logoTable.querySelector(`thead td:first-child`).innerText;
+
+            // Если номер группы чётный:
+            if ((groupNumber % 2) === 1) {
+                // Меняем лого на светлый в верхней строке:
+                logosOnLightGrey[i].setAttribute('src', `../../images/${logoCode(logosOnLightGrey[i])}_dark.png`);
+
+                // Определяем строку нашего клуба:
+                const logoRowNumber = +logosOnLightGrey[i + 1].parentElement.previousElementSibling.innerText;
+                // Если номер строки нечётный, то уходим:
+                if ((logoRowNumber % 2) === 1) continue;
+                // Если же чётный:
+                else {
+                    logosOnLightGrey[i + 1].setAttribute('src', `../../images/${logoCode(logosOnLightGrey[i + 1])}_dark.png`);
+                    logosOnLightGrey[i + 2].setAttribute('src', `../../images/${logoCode(logosOnLightGrey[i + 2])}_dark.png`);
+                }
+            }
+        }
+    }    
+    
+});
