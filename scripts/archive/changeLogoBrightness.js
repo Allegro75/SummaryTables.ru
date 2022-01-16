25b00222fdf6624a0d53d5f143463884ddc3f876
Allegro75 <otkidach75@yandex.ru>
2022-01-16 04:52:42 +0300

    AuW_light

--- scripts/archive/changeLogoBrightness.js
+++ scripts/archive/changeLogoBrightness.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Ищем логотипы для ЗАМЕНЫ на СВЕТЛОЕ в групповых таблицах:
     const queryPart = `table img.football-logo-table[src='../../images/`;
-    const logosToLightInTables = document.querySelectorAll(`${queryPart}Zen.png'], ${queryPart}Mar.png'], ${queryPart}Nan.png'], ${queryPart}And_dark.png'], ${queryPart}DyK.png'], ${queryPart}Mon.png'], ${queryPart}Mlm.png'], ${queryPart}RSo.png'], ${queryPart}RaW.png']`);
+    const logosToLightInTables = document.querySelectorAll(`${queryPart}Zen.png'], ${queryPart}Mar.png'], ${queryPart}Nan.png'], ${queryPart}And_dark.png'], ${queryPart}DyK.png'], ${queryPart}Mon.png'], ${queryPart}Mlm.png'], ${queryPart}RSo.png'], ${queryPart}RaW.png'], ${queryPart}AuW.png']`);
     // console.log(logosToLightInTables)
     // Доп. массив для логотипов, нуждающихся в замене на светлый вариант даже на светло-сером фоне:
     const additionalArray = [`Mon`, `Zen`, `Mar`];
