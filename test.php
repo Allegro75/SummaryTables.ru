8db1b626547207d41540da7c069d66aec5f75d1e
Allegro75 <otkidach75@yandex.ru>
2022-02-21 02:58:10 +0300

    test-2

--- test.php
+++ test.php
@@ -1,2 +1,41 @@
-<?
-var_dump("test");
+<!DOCTYPE html>
+<html lang="ru" class="football">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="author" content="Edwards, Allegro, Edwards75, Allegro75, Oleg Otkidach">
+    <meta name="author" content="Олег Откидач">
+    <meta name="description" content="Сводная таблица истории противостояний европейских футбольных клубов в рамках еврокубков.
+    Фавориты Лиги чемпионов 2021/2022">
+    <meta name="keywords" content="Футбол. Еврокубки. Европа. 
+    Личные встречи. Личные счета. vs. История игр. История противостояний.  
+    Сводная таблица. Таблица-'шахматка'. Статистика. История. Результаты.
+    Клубы. Суперклубы. Команды.
+    Реал. Барселона. Ливерпуль. Манчестер Юнайтед. Арсенал. Бавария. Ювентус. Аякс.
+    Лига чемпионов. Кубок чемпионов. Лига Европы. Кубок УЕФА. Кубок кубков. Суперкубок.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/football_ball.svg" type="image/x-icon">
+    <title>Фавориты ЛЧ. Личные счета суперклубов. Сводная таблица</title>
+    <link rel="stylesheet" href="stylesheets/football__body.css">
+    <link rel="stylesheet" href="stylesheets/cap-wo-nav.css">
+    <link rel="stylesheet" href="stylesheets/navigation.css">
+    <link rel="stylesheet" href="stylesheets/captions.css">
+    <link rel="stylesheet" href="stylesheets/table16.css">
+    <link rel="stylesheet" href="stylesheets/duel-button-lower.css">
+    <link rel="stylesheet" href="stylesheets/settings.css">
+    <link rel="stylesheet" href="stylesheets/donate.css">
+    <link rel="stylesheet" href="stylesheets/footer.css">
+
+</head>
+
+<body class="football__body">
+    <div class="football__background">
+
+    <?
+        var_dump("test");
+    ?>
+
+    </div>
+</body>
+</html>
