eaaac35a26378a2985fe9711369a44ee80350464
Allegro75 <otkidach75@yandex.ru>
2022-02-21 03:10:40 +0300

    test-4

--- test.php
+++ test.php
@@ -6,24 +6,20 @@
     <meta charset="utf-8">
     <meta name="author" content="Edwards, Allegro, Edwards75, Allegro75, Oleg Otkidach">
     <meta name="author" content="Олег Откидач">
-    <meta name="description" content="Сводная таблица истории противостояний европейских футбольных клубов в рамках еврокубков.
-    Фавориты Лиги чемпионов 2021/2022">
+    <meta name="description" content="История противостояний европейских футбольных клубов в рамках еврокубков.">
     <meta name="keywords" content="Футбол. Еврокубки. Европа. 
     Личные встречи. Личные счета. vs. История игр. История противостояний.  
-    Сводная таблица. Таблица-'шахматка'. Статистика. История. Результаты.
+    Статистика. История. Результаты.
     Клубы. Суперклубы. Команды.
     Реал. Барселона. Ливерпуль. Манчестер Юнайтед. Арсенал. Бавария. Ювентус. Аякс.
     Лига чемпионов. Кубок чемпионов. Лига Европы. Кубок УЕФА. Кубок кубков. Суперкубок.">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="images/football_ball.svg" type="image/x-icon">
-    <title>Фавориты ЛЧ. Личные счета суперклубов. Сводная таблица</title>
+    <title>Выбрать пару. История встреч.</title>
     <link rel="stylesheet" href="stylesheets/football__body.css">
     <link rel="stylesheet" href="stylesheets/cap-wo-nav.css">
     <link rel="stylesheet" href="stylesheets/navigation.css">
     <link rel="stylesheet" href="stylesheets/captions.css">
-    <!-- <link rel="stylesheet" href="stylesheets/table16.css"> -->
-    <!-- <link rel="stylesheet" href="stylesheets/duel-button-lower.css"> -->
-    <!-- <link rel="stylesheet" href="stylesheets/settings.css"> -->
     <link rel="stylesheet" href="stylesheets/donate.css">
     <link rel="stylesheet" href="stylesheets/footer.css">
 
@@ -227,11 +223,39 @@
                         </li>
             </nav>
 
-        </header>    
+        </header> 
+        
+        <main>
 
-        <?
-            var_dump("test");
-        ?>
+            <!-- Заголовки -->
+            <section class="captions">
+                <h2 class="captions__h2">
+                    ИСТОРИЯ ПРОТИВОСТОЯНИЙ ФУТБОЛЬНЫХ КЛУБОВ В ЕВРОКУБКАХ
+                </h2>
+
+                <p class="captions__explanation">
+                    <span class="captions__explanation_circle">&#8226;</span>
+                    Учитывались матчи только в рамках официальных европейских клубных турниров:
+                    кубка чемпионов, лиги чемпионов, кубка кубков, кубка ярмарок, кубка УЕФА, лиги Европы, суперкубка
+                    <span class="captions__explanation_circle">&#8226;</span>
+                </p>
+
+                <h1 class="captions__h1">
+                    ПАРА КЛУБОВ НА ВАШ ВЫБОР
+                </h1>
+
+                <p class="captions__explanation">
+                    <span class="captions__explanation_circle">&#8226;</span>                
+                        Учтены матчи до 17.02.2022 включительно
+                    <span class="captions__explanation_circle">&#8226;</span>
+                </p>  
+            </section>        
+
+            <?
+                var_dump("test");
+            ?>
+
+        </main>
 
     </div>
 </body>
