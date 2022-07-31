dca3d3a8504b03f44f213296f220f34282d75f35
Allegro75 <otkidach75@yandex.ru>
2022-07-31 20:33:30 +0300

    ws-5

--- elefly/socket_1.php
+++ elefly/socket_1.php
@@ -1,21 +1,21 @@
 #!/usr/bin/env php
 <?php
 
-// $socket = stream_socket_server("tcp://0.0.0.0:2000", $errno, $errstr);
+$socket = stream_socket_server("tcp://0.0.0.0:2000", $errno, $errstr);
 
-// if (!$socket) {
-//     die("$errstr ($errno)\n");
-// }
+if (!$socket) {
+    die("$errstr ($errno)\n");
+}
 
-// while ($connect = stream_socket_accept($socket, -1)) {
-//     fwrite($connect, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\n\r\nСпартак");
-//     fclose($connect);
-// }
+while ($connect = stream_socket_accept($socket, -1)) {
+    fwrite($connect, "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nConnection: close\r\n\r\nСпартак");
+    fclose($connect);
+}
 
-// fclose($socket);
+fclose($socket);
 
-$SecWebSocketAccept = base64_encode(pack('H*', sha1($SecWebSocketKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
-$response = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
-    "Upgrade: websocket\r\n" .
-    "Connection: Upgrade\r\n" .
-    "Sec-WebSocket-Accept:$SecWebSocketAccept\r\n\r\n";
+// $SecWebSocketAccept = base64_encode(pack('H*', sha1($SecWebSocketKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
+// $response = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
+//     "Upgrade: websocket\r\n" .
+//     "Connection: Upgrade\r\n" .
+//     "Sec-WebSocket-Accept:$SecWebSocketAccept\r\n\r\n";
