2e91bc4d79d123364c5a8a9bff48aee440bc68b4
Allegro75 <otkidach75@yandex.ru>
2022-08-14 21:21:33 +0300

    corr2

--- chess_engine/index.php
+++ chess_engine/index.php
@@ -1,3 +1,27 @@
+
 <?
+
 // phpinfo();
-echo "Spartak";
+// echo "Spartak";
+
+$actualPosition = [
+    "whitePiecesPositions" => [
+        "king" => [
+            "vertical" => "h",
+            "horizontal" => "6",
+        ],
+        "rooks" => [
+            0 => [
+                "vertical" => "a",
+                "horizontal" => "7",
+            ],
+        ],
+    ],
+    "blackPiecesPositions" => [
+        "king" => [
+            "vertical" => "h",
+            "horizontal" => "8",
+        ],
+    ],
+];
+var_dump($actualPosition);
