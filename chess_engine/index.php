b02a9b471cbde1ef947899d589f791cb42f400ae
Allegro75 <otkidach75@yandex.ru>
2022-08-22 01:10:29 +0300

    cec9

--- chess_engine/index.php
+++ chess_engine/index.php
@@ -41,7 +41,7 @@ echo "<pre>";
 // var_dump($bKing_h8->getAccesibleCells());
 // var_dump($wKing_h6->getColor());
 // var_dump($bKing_h8->getColor());
-var_dump($actualPicesSet);
+// var_dump($actualPicesSet);
 // var_dump($actualPicesSet->actualPiecesSet["whites"]["king"]->getAccesibleCells());
 // var_dump($actualPicesSet->actualPiecesSet["blacks"]["king"]->getAccesibleCells());
 var_dump($actualPicesSet->actualPiecesSet["whites"]["rooks"][0]->getAccesibleCells());
