33aaccb8c346d1dd0c6de46fb84974e2051f88e9
Allegro75 <otkidach75@yandex.ru>
2022-09-18 20:48:49 +0300

    p3

--- parser/jsObjectsGetter.php
+++ parser/jsObjectsGetter.php
@@ -6,12 +6,28 @@ if (true) {
     // $originalFileContent = file_get_contents("https://www.championat.com/football/_ucl/tournament/4993/calendar/");
     // echo $originalFileContent;
 
-    $ch = curl_init("https://www.championat.com/football/_ucl/tournament/4993/calendar/");
+    $headers = array(
+        'cache-control: max-age=0',
+        'upgrade-insecure-requests: 1',
+        'user-agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
+        'sec-fetch-user: ?1',
+        'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'x-compress: null',
+        'sec-fetch-site: none',
+        'sec-fetch-mode: navigate',
+        'accept-encoding: deflate, br',
+        'accept-language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    );
+     
+    $ch = curl_init('https://example.com');
+    curl_setopt($ch, CURLOPT_COOKIEFILE, __DIR__ . '/cookie.txt');
+    curl_setopt($ch, CURLOPT_COOKIEJAR, __DIR__ . '/cookie.txt');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_HEADER, true);
     $html = curl_exec($ch);
     curl_close($ch);
-    
+     
     echo $html;
 }
