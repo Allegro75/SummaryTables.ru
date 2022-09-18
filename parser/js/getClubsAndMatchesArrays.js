26b849f45c5a05be669770a07bd9359b668512e0
Allegro75 <otkidach75@yandex.ru>
2022-09-19 00:31:07 +0300

    p18

--- parser/js/getClubsAndMatchesArrays.js
+++ parser/js/getClubsAndMatchesArrays.js
@@ -10,24 +10,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (rowInd > 0) {
 
-            console.log('стадия:\n') // стадия
-            console.log(row.cells[3].textContent) // стадия
-            console.log('\n') // стадия
+            if (row.cells[7].textContent.trim() !== '– : –') { // Если матч сыгран
 
-            console.log('дата:\n') // дата
-            console.log(row.cells[5].textContent.trim().substr(0, 10)) // дата
-            console.log('\n') // дата
+                console.log('стадия:\n') // стадия
+                console.log(row.cells[3].textContent) // стадия
+                // console.log('\n') // стадия
 
-            // console.log(row.cells[6].textContent) // клубы-участники
-            const clubNames = row.cells[6].querySelectorAll('span.table-item__name')
-            console.log('клубы-участники:\n') // клубы-участники
-            console.log(clubNames[0].textContent) // клубы-участники
-            console.log(clubNames[1].textContent) // клубы-участники
-            console.log('\n')
+                console.log('дата:\n') // дата
+                console.log(row.cells[5].textContent.trim().substr(0, 10)) // дата
+                // console.log('\n') // дата
 
-            console.log('счёт:\n') // счёт
-            console.log(row.cells[7].textContent.trim()) // счёт
-            console.log('\n')
+                // console.log(row.cells[6].textContent) // клубы-участники
+                const clubNames = row.cells[6].querySelectorAll('span.table-item__name')
+                console.log('клубы-участники:\n') // клубы-участники
+                console.log(clubNames[0].textContent) // клубы-участники
+                console.log(clubNames[1].textContent) // клубы-участники
+                // console.log('\n')
+
+                console.log('счёт:\n') // счёт
+                console.log(row.cells[7].textContent.trim()) // счёт
+                console.log('\n')
+
+            }
 
         }
         
