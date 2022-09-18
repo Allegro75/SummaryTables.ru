402e06ef0cd295a37d921e604c4ae65c2b5b55f2
Allegro75 <otkidach75@yandex.ru>
2022-09-18 20:47:40 +0300

    p2

--- parser/jsObjectsGetter.php
+++ parser/jsObjectsGetter.php
@@ -2,5 +2,16 @@
 <?
 
 // Получаем веб-страницу из Сети
-$originalFileContent = file_get_contents("https://www.championat.com/football/_ucl/tournament/4993/calendar/");
-echo $originalFileContent;
+if (true) {
+    // $originalFileContent = file_get_contents("https://www.championat.com/football/_ucl/tournament/4993/calendar/");
+    // echo $originalFileContent;
+
+    $ch = curl_init("https://www.championat.com/football/_ucl/tournament/4993/calendar/");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    $html = curl_exec($ch);
+    curl_close($ch);
+    
+    echo $html;
+}
