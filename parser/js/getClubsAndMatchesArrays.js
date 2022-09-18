626794b43251b4247d43d14b3f51d4e26ef1ba92
Allegro75 <otkidach75@yandex.ru>
2022-09-18 21:35:57 +0300

    p14

--- parser/js/getClubsAndMatchesArrays.js
+++ parser/js/getClubsAndMatchesArrays.js
@@ -1,13 +1,10 @@
 
-console.log('yes')
-
 document.addEventListener('DOMContentLoaded', () => {
 
-    let clubsArr = []
-    let matchesArr = []
+    let clubsArr = {}
+    let matchesArr = {}
 
     const bigTable = document.querySelector('.stat-results__table')
-    // console.log(bigTable)
     const tableRowsArr = Array.from(bigTable.rows);
     tableRowsArr.forEach((row, rowInd) => {
 
