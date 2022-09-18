4b6c76f835aaab658d2d5331dead2eba23926609
Allegro75 <otkidach75@yandex.ru>
2022-09-19 00:11:13 +0300

    p14

--- parser/js/getClubsAndMatchesArrays.js
+++ parser/js/getClubsAndMatchesArrays.js
@@ -9,7 +9,10 @@ document.addEventListener('DOMContentLoaded', () => {
     tableRowsArr.forEach((row, rowInd) => {
 
         if (rowInd > 0) {
-            console.log(row.cells[3].textContent)
+            console.log(row.cells[3].textContent) // стадия
+            console.log(row.cells[5].textContent) // дата
+            console.log(row.cells[6].textContent) // клубы-участники
+            console.log(row.cells[7].textContent) // счёт
         }
         
     });
