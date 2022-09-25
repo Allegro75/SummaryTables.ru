036fed1600763d080bb218a7f87bbd2f7bd10876
Allegro75 <otkidach75@yandex.ru>
2022-09-25 22:12:18 +0300

    p45

--- parser/js/getClubsAndMatchesArrays.js
+++ parser/js/getClubsAndMatchesArrays.js
@@ -72,7 +72,7 @@ document.addEventListener('DOMContentLoaded', async function() {
       
       // console.log(response);
       let result = await response.json();
-      console.log(result);
+      console.log(result[0]);
       // console.log(result.yes);
       // console.log(result.message);
 
