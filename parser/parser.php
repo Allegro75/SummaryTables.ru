cc46aa4ecd5fb07230fb5a3b5ea9b7c3a9e3cf5b
Allegro75 <otkidach75@yandex.ru>
2022-09-25 22:22:58 +0300

    p53

--- parser/parser.php
+++ parser/parser.php
@@ -11,4 +11,5 @@ $clubs = json_decode(file_get_contents('php://input'), true);
 // echo json_encode($arr);
 // echo json_encode($_POST['body']);
 // echo json_encode($_POST);
-echo $clubs;
+echo json_encode($clubs);
+
