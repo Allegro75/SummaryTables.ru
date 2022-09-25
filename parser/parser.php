9ce0542f1ba2f5217849285c34c11d9fdc515744
Allegro75 <otkidach75@yandex.ru>
2022-09-25 22:21:43 +0300

    p52

--- parser/parser.php
+++ parser/parser.php
@@ -6,6 +6,9 @@
 //     'message' => 2,
 // ];
 
+$clubs = json_decode(file_get_contents('php://input'), true);
+
+// echo json_encode($arr);
 // echo json_encode($_POST['body']);
 // echo json_encode($_POST);
-echo json_encode('php://input');
+echo $clubs;
