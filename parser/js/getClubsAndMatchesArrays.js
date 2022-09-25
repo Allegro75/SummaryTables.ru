a665898c153019163aa663f7cacbfd01ca43b667
Allegro75 <otkidach75@yandex.ru>
2022-09-25 19:38:09 +0300

    p22

--- parser/js/getClubsAndMatchesArrays.js
+++ parser/js/getClubsAndMatchesArrays.js
@@ -11,28 +11,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (rowInd > 0) {
 
-            if (row.cells[7].textContent.trim() !== '– : –') { // Если матч сыгран
+            // if (row.cells[7].textContent.trim() !== '– : –') { // Если матч сыгран
 
-                console.log('стадия:\n') // стадия
-                console.log(row.cells[3].textContent) // стадия
-                // console.log('\n') // стадия
+            //     console.log('стадия:\n') // стадия
+            //     console.log(row.cells[3].textContent) // стадия
 
-                console.log('дата:\n') // дата
-                console.log(row.cells[5].textContent.trim().substr(0, 10)) // дата
-                // console.log('\n') // дата
+            //     console.log('дата:\n') // дата
+            //     console.log(row.cells[5].textContent.trim().substr(0, 10)) // дата
 
-                // console.log(row.cells[6].textContent) // клубы-участники
-                const clubNames = row.cells[6].querySelectorAll('span.table-item__name')
-                console.log('клубы-участники:\n') // клубы-участники
-                console.log(clubNames[0].textContent) // клубы-участники
-                console.log(clubNames[1].textContent) // клубы-участники
-                // console.log('\n')
+            //     const clubNames = row.cells[6].querySelectorAll('span.table-item__name')
+            //     console.log('клубы-участники:\n') // клубы-участники
+            //     console.log(clubNames[0].textContent) // клубы-участники
+            //     console.log(clubNames[1].textContent) // клубы-участники
 
-                console.log('счёт:\n') // счёт
-                console.log(row.cells[7].textContent.trim()) // счёт
-                console.log('\n')
+            //     console.log('счёт:\n') // счёт
+            //     console.log(row.cells[7].textContent.trim()) // счёт
+            //     console.log('\n')
 
-            }
+            // }
 
         }
         
@@ -43,6 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (response.ok) { // если HTTP-статус в диапазоне 200-299      
       let json = await response.json();
+      console.log(response);
     } else {
       alert("Ошибка HTTP: " + response.status);
     }    
