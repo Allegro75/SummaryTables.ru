51bd960178b4134428da358cfc7bd2cd12272183
Allegro75 <otkidach75@yandex.ru>
2022-10-02 22:48:47 +0300

    p22

--- parser/parser.php
+++ parser/parser.php
@@ -48,6 +48,48 @@ if (true) {
             $clubsList['newClubs'][] = $curClub;
         }
     }
-    echo json_encode($clubsList);
+    // echo json_encode($clubsList);
 
 }
+
+// Определяем места проведения матчей
+$netMatchesArr = [];
+foreach ($clubsAndMatchesArr['matches'] as $curMatch) {
+
+    // Получаем веб-страницу из Сети
+    if (true) {
+        // $originalFileContent = file_get_contents("https://www.championat.com/football/_ucl/tournament/4993/calendar/");
+        // echo $originalFileContent;
+
+        $originalFileContent = '';
+        $headers = array(
+            'cache-control: max-age=0',
+            'upgrade-insecure-requests: 1',
+            'user-agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
+            'sec-fetch-user: ?1',
+            'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            'x-compress: null',
+            'sec-fetch-site: none',
+            'sec-fetch-mode: navigate',
+            'accept-encoding: deflate, br',
+            'accept-language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        );
+        
+        $ch = curl_init("https://www.championat.com{$curMatch['matchWebPage']}");
+        curl_setopt($ch, CURLOPT_COOKIEFILE, __DIR__ . '/cookie.txt');
+        curl_setopt($ch, CURLOPT_COOKIEJAR, __DIR__ . '/cookie.txt');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        $originalFileContent = curl_exec($ch);
+        curl_close($ch);        
+        // echo $originalFileContent;
+
+        $approximateNeededNodeIndex = mb_strpos($originalFileContent, 'wghxp match-info__extra-row');
+        $netMatchesArr[] = $approximateNeededNodeIndex;
+
+    }
+
+}
+echo json_encode($netMatchesArr);
