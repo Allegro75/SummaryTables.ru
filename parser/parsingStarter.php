930987fb63d0e7c89f10ecb978ba98c675c7a555
Allegro75 <otkidach75@yandex.ru>
2022-10-15 18:19:52 +0300

    p15

--- parser/parsingStarter.php
+++ parser/parsingStarter.php
@@ -1,6 +1,8 @@
 
 <?
 
+phpversion();
+
 // Получаем веб-страницу из Сети
 if (true) {
     // $originalFileContent = file_get_contents("https://www.championat.com/football/_ucl/tournament/4993/calendar/");
