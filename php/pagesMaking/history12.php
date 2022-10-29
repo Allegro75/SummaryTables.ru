cb61a881148d886d05306e7ec37b4d4ba6e27384
Allegro75 <otkidach75@yandex.ru>
2022-10-29 22:10:38 +0300

    m1

---
+++ php/pagesMaking/history12.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ru" class="football">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="author" content="Edwards, Allegro, Edwards75, Allegro75, Oleg Otkidach">
+    <meta name="author" content="Олег Откидач">
+    <meta name="description" content="Сводная таблица истории противостояний европейских футбольных клубов в рамках еврокубков. 12 лучших клубов.">
+    <meta name="keywords" content="Футбол. Еврокубки. Европа. 
+    Личные встречи. Личные счета. vs. История игр. История противостояний.  
+    Сводная таблица. Таблица-'шахматка'. Статистика. История. Результаты. 
+    Клубы. Суперклубы. Команды. 
+    Реал. Барселона. Ливерпуль. Манчестер Юнайтед. Бавария. Ювентус. Аякс. Милан. Интер.
+    Лига чемпионов. Кубок чемпионов. Лига Европы. Кубок УЕФА. Кубок кубков. Суперкубок.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="images/football_ball.svg" type="image/x-icon">
+    <title>История. Личные счета суперклубов. Сводная таблица</title>
+    <link rel="stylesheet" href="stylesheets/football__body.css">
+    <link rel="stylesheet" href="stylesheets/cap-wo-nav.css">
+    <link rel="stylesheet" href="stylesheets/navigation.css">
+    <link rel="stylesheet" href="stylesheets/captions.css">
+    <link rel="stylesheet" href="stylesheets/table12.css">
+    <link rel="stylesheet" href="stylesheets/duel-button-lower.css">
+    <link rel="stylesheet" href="stylesheets/settings.css">
+    <link rel="stylesheet" href="stylesheets/donate.css">
+    <link rel="stylesheet" href="stylesheets/footer.css">
+
+</head>
+
+<body class="football__body">
+
+    <div class="football__background">
+
+    <!-- Шапка, две оранжевые полосы с навигацией: -->
+    <? require_once 'layoutElements/header/shortHeader.php'; ?>
+
+
+
+
+    </div class="football__background">
+
+    <script src="scripts/showSmallTableOnCellClick.js"></script>
+    <script src="scripts/showHideDuels.js"></script>
+    <script src="scripts/cellsColoring.js"></script>
+    <script src="scripts/coloringOnLoad.js"></script>
+    <script src="scripts/randPairOnLoad.js"></script>
+    <script src="scripts/randomWindowOnClick.js"></script>
+    <script src="scripts/duelsOnLoad.js"></script>
+    <!-- <script src="scripts/service/service_rebuildTable.js"></script> -->
+    
+</body>
+
+</html>    
---
+++ php/pagesMaking/layoutElements/header/shortHeader.php
@@ -0,0 +1,212 @@
+        
+        <? // Краткий header - без блока со ссылками на турниры по годам - для всех страниц, кроме архива турниров ?>
+
+        <!-- Шапка -->
+        <header class="cap">
+
+            <div class="cap__site-title-stripe site-title-stripe">
+
+                <a href="../../index.html" title="Лучшие клубы Европы за всю историю. Личные встречи. Сводная таблица.">
+                    <img class="site-title-stripe__icon  site-title-stripe__icon_left" src="../../images/football_ball_yellow.svg" alt="футбол">
+                </a>
+
+                <a href="../../index.html" title="Лучшие клубы Европы за всю историю. Личные встречи. Сводная таблица.">
+                    <img class="site-title-stripe__icon  site-title-stripe__icon_right" src="../../images/football_ball_yellow.svg" alt="футбол">
+                </a>
+
+                <div class="site-title-stripe__text">
+                    <a href="../../index.html" title="Лучшие клубы Европы за всю историю. Личные встречи. Сводная таблица.">
+                        Футбол <span class="site-title-stripe__text_delimiter">&#8226;</span>
+                        Суперклубы <span class="site-title-stripe__text_delimiter">&#8226;</span>
+                        Истории личных встреч <span class="site-title-stripe__text_delimiter">&#8226;</span>
+                        Сводные таблицы
+                    </a>
+                </div>
+
+            </div>
+
+            <nav class="nav">
+                <div class="nav__stripe football">
+                    <!-- <div class="nav__stripe__title">
+                    <a href="../../history36.html" title="Лучшие клубы Европы. Личные встречи. Сводная таблица">
+                        ФУТБОЛ:
+                    </a>
+                    </div> -->
+                    <ul class="nav__stripe__list">
+                        
+						<li class="nav__stripe__list__item select-pair">
+                            <a href="../../selected-pair.php" title="Узнать историю встреч пары клубов на свой выбор">
+                                <div class="nav__icon select-pair">
+                                    <!-- Сюда через css подставляется иконка -->
+                                </div>
+                                Выбрать пару
+                            </a>
+                        </li>
+
+						<li class="nav__stripe__list__item  dropdown  current-season">
+                            <a href="../../champ_league_current.html"
+                                title="Фавориты еврокубков текущего сезона. Личные встречи. Сводная таблица">
+                                <div class="nav__icon current">
+                                </div>
+                                ТЕКУЩИЙ СЕЗОН
+                                &#8729; Сводные таблицы
+                            </a>
+
+                            <ul class="nav__stripe__list__item__droplist">
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../champ_league/cl_2023.html"
+                                        title="Лига чемпионов 2022/2023. Ход турнира">
+                                        ЛЧ 2022/2023. Ход турнира
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../champ_league_current.html" title="Фавориты Лиги чемпионов 2021/2022">
+                                        Фавориты ЛЧ 2021/2022
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../euroleague/el_2023.html"
+                                        title="Лига Европы 2022/2023. Ход турнира">
+                                        ЛЕ 2022/2023. Ход турнира
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../euroleague_current.html" title="Фавориты Лиги Европы 2021/2022">
+                                        Фавориты ЛЕ 2021/2022
+                                    </a>
+                                </li>
+                            </ul>
+
+                        </li>
+                        <li class="nav__stripe__list__item  dropdown  ussr">
+                            <a href="../../russia.html"
+                                title="Российские (и советские) клубы против европейских. Личные встречи. Сводная таблица">
+                                <div class="nav__icon ussr">
+                                    <!-- <img src="../../images/ussr.png" alt="Россия/СССР" class="nav__icon ussr nav__icon_yellow">
+                         <img src="../../images/ussr_black.png" alt="Россия/СССР"
+                            class="nav__icon ussr nav__icon_black"> -->
+                                </div>
+                                РОССИЯ / СССР
+                            </a>
+                            <ul class="nav__stripe__list__item__droplist">
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../russia.html" title="Российские клубы против европейских">
+                                        Россия
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../ukraine.html" title="Украинские клубы против европейских">
+                                        Украина
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../byelorussia.html" title="Белорусские клубы против европейских">
+                                        Белоруссия
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../kazakhstan.html"
+                                        title="Казахстанские клубы против европейских. Личные встречи. Сводная таблица">
+                                        Казахстан
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav__stripe__list__item  dropdown  history">
+                            <a href="../../history36.html"
+                                title="Лучшие клубы Европы за всю историю. Личные встречи. Сводная таблица">
+                                <div class="nav__icon history">
+                                    <!-- <img src="./images/history_icon.png" alt="история"
+                            class="nav__icon history nav__icon_yellow">
+                         <img src="./images/history_icon_black.png" alt="история"
+                            class="nav__icon history nav__icon_black"> -->
+                                </div>
+                                вся ИСТОРИЯ
+                            </a>
+                            <ul class="nav__stripe__list__item__droplist">
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../history.html" title="12 лучших клубов Европы за всю историю">
+                                        <span class="figures-in-history-hrefs">12</span> клубов
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../history24.html" title="24 лучших клуба Европы за всю историю"> <span
+                                            class="figures-in-history-hrefs">24</span> клуба </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../history36.html" title="36 лучших клубов Европы за всю историю"> <span
+                                            class="figures-in-history-hrefs">36</span> клубов </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../winners.html"
+                                        title="Все 22 победителя Лиги/кубка чемпионов всех лет">
+                                        Победители ЛЧ
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../../range.html" title="Список 100 лучших клубов Европы">
+                                        Ранжир
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="nav__stripe__list__item  decade">
+                            <a href="../../decade.html"
+                                title="24 лучших клуба десятилетия (2013-2022). Личные встречи. Сводная таблица">
+                                <div class="nav__icon decade">
+                                    <!-- <img src="./images/ten.png" alt="десятилетие" class="nav__icon decade nav__icon_yellow">
+                         <img src="./images/ten_black.png" alt="десятилетие"
+                            class="nav__icon decade nav__icon_black"> -->
+                                </div>
+                                Десятилетие
+                            </a>
+                        </li>
+
+                        <li class="nav__stripe__list__item  range">
+                            <a href="../../range.html" title="Список 100 лучших клубов в истории">
+                                <div class="nav__icon range">
+                                    <!-- Сюда через css подставляется иконка -->
+                                </div>
+                                Ранжир
+                            </a>
+                        </li>
+
+                        
+
+                        <li class="nav__stripe__list__item  dropdown  archive">
+
+                            <a href="../champ_league/cl_2023.html" title="Еврокубки, история. Ход турниров">
+                                <div class="nav__icon history"></div>
+                                АРХИВ ТУРНИРОВ
+                            </a>
+
+                            <ul class="nav__stripe__list__item__droplist">
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../champ_league/cl_2023.html"
+                                        title="Лига/кубок чемпионов, история. Ход турниров">
+                                        Лига чемпионов
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../cup_win_cup/cwc_1999.html" title="Кубок кубков, история. Ход турниров">
+                                        Кубок кубков
+                                    </a>
+                                </li>
+                                <li class="nav__stripe__list__item__droplist__item">
+                                    <a href="../euroleague/el_2023.html"
+                                        title="Лига Европы / кубок УЕФА / кубок ярмарок, история. Ход турниров">
+                                        Лига Европы
+                                    </a>
+                                </li>
+                            </ul>
+
+                        </li>
+
+                    </ul>
+                </div>
+
+            </nav>
+
+        </header>
+        
