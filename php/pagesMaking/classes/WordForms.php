1e054de92c8d02777b611f58d20e25fe72c3974e
Allegro75 <otkidach75@yandex.ru>
2022-10-30 20:57:48 +0300

    m52

--- php/pagesMaking/classes/WordForms.php
+++ php/pagesMaking/classes/WordForms.php
@@ -24,4 +24,23 @@ class WordForms
 
     }
 
+    // Получение правильной формы слова типа "ничья" (женский род, окончание на "я") после числительного
+    public static function getWordLikeDraw($opts = [])
+    {
+
+        $number = $opts["number"];
+        $word = $opts["word"];
+
+        $wordBase = mb_substr($word, 0, (mb_strlen($word) - 1)); // Основа, корень слова. Это слово без последнего символа (например, "ничь" для исходного слова "ничья")
+        $drawWord = "{$wordBase}их";
+        if ($number === 1) {
+            $drawWord = $word;
+        } else if (($number >= 2) && ($number <= 4)) {
+            $drawWord = "{$wordBase}и";
+        }
+        
+        return $drawWord;
+
+    }
+
 }
