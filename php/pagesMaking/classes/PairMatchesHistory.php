553b48cfde77909743b8818e992121aa1adacd9d
Allegro75 <otkidach75@yandex.ru>
2022-10-30 18:21:18 +0300

    m12

--- php/pagesMaking/classes/PairMatchesHistory.php
+++ php/pagesMaking/classes/PairMatchesHistory.php
@@ -25,7 +25,7 @@ class PairMatchesHistory {
 
         $history = [];
 
-        // Определяем статистику:
+        // Определяем набор матчей данной пары:
         $sql =
             "SELECT * 
             FROM `matches` 
@@ -43,18 +43,56 @@ class PairMatchesHistory {
                 )
             AND `score` != ''
         ";
-        $result = mysqli_query($this->db, $sql);
         $matchesArr = array();
-        if (mysqli_num_rows($result) > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-                $matchesArr[] = $row;
+        if ($result = mysqli_query($this->db, $sql)) {        
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $matchesArr[] = $row;
+                }
             }
         }
         $noHistory = false;
         if (count($matchesArr) === 0) {
             $noHistory = true;
         }
-        $history = count($matchesArr);
+        // $history = count($matchesArr);
+
+                // Про победы, ничьи, поражения:
+                if (true) {
+
+                    $firstVictories = 0;
+                    $draws = 0;
+                    for ($i = 0; $i < count($matchesArr); $i++) {
+                        $draws += $matchesArr[$i]['fCDraw'];
+                        if ($matchesArr[$i]['firstClubId'] == $firstClubId) {
+                            $firstVictories += $matchesArr[$i]['fCVictory'];
+                        } else {
+                            $firstVictories += $matchesArr[$i]['fCLesion'];
+                        }
+                    }
+                    $firstLesions = count($matchesArr) - $firstVictories - $draws;
+                    $history = [
+                        "firstVictories" => $firstVictories,
+                        "draws" => $draws,
+                        "firstLesions" => $firstLesions,
+                    ];
+
+                    // Про разницу мячей:
+                    $firstGoals = 0;
+                    $secondGoals = 0;
+                    for ($i = 0; $i < count($matchesArr); $i++) {
+                        if ($matchesArr[$i]['firstClubId'] == $firstClubId) {
+                            $firstGoals += $matchesArr[$i]['firstClubGoals'];
+                            $secondGoals += $matchesArr[$i]['secondClubGoals'];
+                        } else {
+                            $firstGoals += $matchesArr[$i]['secondClubGoals'];
+                            $secondGoals += $matchesArr[$i]['firstClubGoals'];
+                        }
+                    }
+                    $history["firstGoals"] = $firstGoals;
+                    $history["secondGoals"] = $secondGoals;
+
+                }        
 
         return $history;
 
