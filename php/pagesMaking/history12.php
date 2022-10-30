4b646a0aa6d48fafd4623cb52ec11a63fd3a1271
Allegro75 <otkidach75@yandex.ru>
2022-10-30 20:36:27 +0300

    m45

--- php/pagesMaking/history12.php
+++ php/pagesMaking/history12.php
@@ -176,6 +176,7 @@
 
                                     <td id="<?=$curPairCode?>" class="statistics <?=$curPairHasHistoryClass?>">
                                         <div class="results" title="<?=$curClubInfo["shortName"]?> - <?=$innerCycleClubInfo["shortName"]?>
+                                        
 <?=$curPairHistory["firstVictories"]?> победы, <?=$curPairHistory["draws"]?> ничьи, <?=$curPairHistory["firstLesions"]?> поражения
 Кликните, чтобы узнать подробности">
                                             <p class="games-score">+<?=$curPairHistory["firstVictories"]?> =<?=$curPairHistory["draws"]?> -<?=$curPairHistory["firstLesions"]?></p>
