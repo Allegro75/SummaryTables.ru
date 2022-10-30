7523c044cb9cc8a9b296ad12e5db74f4ed16bbcd
Allegro75 <otkidach75@yandex.ru>
2022-10-30 15:14:10 +0300

    m9

--- php/pagesMaking/tableInfo.php
+++ php/pagesMaking/tableInfo.php
@@ -9,8 +9,8 @@ function getTableInfo ($opts = []) {
     $rawClubsList = $opts['clubsList'];
 
     $info = [];
-    // $info['rawClubsList'] = $rawClubsList;
 
+    // Получение массива клубов с полной информацией о них из `eurocups_clubs`:
     $clubsList = [];
     foreach (array_keys($rawClubsList) as $curClubName) {
         $newClub = new Club(['pathToRoot' => "../../"]);
@@ -18,7 +18,28 @@ function getTableInfo ($opts = []) {
     }
     $info['clubsList'] = $clubsList;
 
+    // Получение массива ($pairsMatchesHistory) с краткими (представляемыми в итоговой таблице) данными о противостояниях:
+    if (true) {
 
+        $pairsMatchesHistory = [];
+        foreach ($clubsList as $clubName => $clubInfo) { // Перебираем массив клубов
+
+            foreach ($clubsList as $innerCycleClubName => $innerCycleClubInfo) { // Для каждого клуба снова перебираем массив клубов, определяя таким образом рассматриваемые конкретные пары клубов
+                
+                if ($clubName !== $innerCycleClubName) { // Естественно, отбрасываем совпадения перебираемых имён клубов, чтобы пара содержала два разных клуба
+
+                    $pairName = "{$clubName} - {$innerCycleClubName}";
+
+                    $pairsMatchesHistory[$pairName] = [];
+
+                }
+
+            }
+
+        }
+
+    }
+    $info['pairsMatchesHistory'] = $pairsMatchesHistory;
 
     return $info;
 
