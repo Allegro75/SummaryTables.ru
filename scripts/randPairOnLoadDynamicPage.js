51129b13105736165da7d6e038d8d6613d158e89
Allegro75 <otkidach75@yandex.ru>
2022-11-05 21:30:58 +0300

    m18

--- scripts/randPairOnLoadDynamicPage.js
+++ scripts/randPairOnLoadDynamicPage.js
@@ -1,5 +1,5 @@
 
-// То же, что и randPairOnLoad (возможное открытие окна со случайной парой при загрузке страницы), но с ориентацией на динамическую (php) страницу с данными об истории матчей пары 
+// То же, что и randPairOnLoad.js (возможное открытие окна со случайной парой при загрузке страницы), но с ориентацией на динамическую (php) страницу с данными об истории матчей пары 
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -8,10 +8,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const allCellsWHistory = document.querySelectorAll('td.has-history');
         let randomPairIndex = Math.floor( ( allCellsWHistory.length * Math.random() ) );
         const randomPairCell = allCellsWHistory[randomPairIndex];
-        // const randomCellID = randomPairCell.getAttribute('id');
         const firstClubId = randomPairCell.getAttribute(`data-first-club-id`);
         const secClubId = randomPairCell.getAttribute(`data-sec-club-id`);        
-        // const urlInRandomWindow = `football-small-tables/${randomCellID}.html`;
         const urlInRandomWindow = `http://summarytables.ru/selected-pair-small-table.php?club_1=${firstClubId}&club_2=${secClubId}`;
         window.open(urlInRandomWindow, ``, `width=520px, height=970px, top=0, left=0`);
     }
