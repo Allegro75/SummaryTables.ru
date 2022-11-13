ef83cac4cfaa5e699eeb85e699be9e561a7e0586
Allegro75 <otkidach75@yandex.ru>
2022-11-13 17:37:05 +0300

    m3

--- php/pagesMaking/layoutElements/captions/captions.php
+++ php/pagesMaking/layoutElements/captions/captions.php
@@ -7,9 +7,30 @@ function printCaptions ($opts = []) {
     $clubsNumberPhraseLastPart = $opts['clubsNumberPhraseLastPart'];
     $clubsNumber = $opts['clubsNumber'];
 
+    $for36clubsCaptionsPart = ""; // Часть заголовков, используемая только на странице с 36 клубами
+    if ($clubsNumber == 36) {
+        $for36clubsCaptionsPart = 
+        "<p class=\"captions__explanation\">
+            <span class=\"captions__explanation_circle\">&#8226;</span>
+            Счёт в таблице представлен в <em>дуэльном</em> виде.
+            <br> Дуэль - непосредственное столкновение двух команд в ходе турнира.
+            <br> В дуэли побеждает тот, кто проходит дальше в кубковом турнире; либо выигрывает турнир (для
+            финалов); либо для групповых этапов - проходит в стадию более высокую, чем соперник по дуэли.
+            <span class=\"captions__explanation_circle\">&#8226;</span>
+        </p>
+
+        <p class=\"captions__explanation\">
+            <span class=\"captions__explanation_circle\">&#8226;</span>
+            По <em>клику</em> на ячейку таблицы открывается подробная история данной пары, включающая итоги и
+            список результатов в традиционном виде.
+            <span class=\"captions__explanation_circle\">&#8226;</span>
+        </p>";
+    }
+
     echo 
             "<!-- Заголовки -->
             <section class=\"captions\">
+
                 <h2 class=\"captions__h2\">
                     ИСТОРИЯ ПРОТИВОСТОЯНИЙ ФУТБОЛЬНЫХ КЛУБОВ В ЕВРОКУБКАХ. СВОДНАЯ ТАБЛИЦА
                 </h2>
@@ -47,7 +68,10 @@ function printCaptions ($opts = []) {
                         <!-- Таблица обновлена по итогам сезона 2021/2022 -->
                         Учтены матчи до {$lastAccountedMatchDate} включительно (учтены все матчи групповых этапов сезона 2022/2023)
                     <span class=\"captions__explanation_circle\">&#8226;</span>
-                </p>  
+                </p>
+                
+                {$for36clubsCaptionsPart}
+                
             </section>";
 
 }        
