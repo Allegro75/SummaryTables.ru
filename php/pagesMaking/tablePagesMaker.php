8cca425b8b07a2868eb5bfc60edad5ce4574cce2
Allegro75 <otkidach75@yandex.ru>
2022-12-17 20:06:33 +0300

    m23

--- php/pagesMaking/tablePagesMaker.php
+++ php/pagesMaking/tablePagesMaker.php
@@ -266,10 +266,11 @@
 
                 require_once 'tableInfo.php'; // Получение содержания таблицы
                 $tableInfo = getTableInfo (["clubsList" => $clubsList, "actualCountryClubsList" => $actualCountryClubsList ?? [],]);
-                // echo "<pre>";
-                // // var_dump($tableInfo);
+                echo "<pre>";
+                // var_dump($tableInfo);
                 // var_dump($tableInfo['actualCountryClubsList']);
-                // echo "</pre>";
+                var_dump($tableInfo['pairsMatchesHistory']);
+                echo "</pre>";
 
                 require_once 'classes/WordForms.php'; // Файл для получения правильных форм слов
 
