9ffa73bbd38755014aaaf1ecee575c70d658ce5a
Allegro75 <otkidach75@yandex.ru>
2023-06-04 17:01:59 +0300

    u

--- selected-pair.php
+++ selected-pair.php
@@ -281,7 +281,7 @@
 
                 <p class="captions__explanation">
                     <span class="captions__explanation_circle">&#8226;</span>                
-                        Учтены матчи до 18.05.2023 включительно
+                        Учтены матчи до 31.05.2023 включительно
                     <span class="captions__explanation_circle">&#8226;</span>
                 </p>  
             </section>  
