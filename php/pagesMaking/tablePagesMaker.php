6e3fcddfb7103532d14ba2d1c35442c17094110f
Allegro75 <otkidach75@yandex.ru>
2023-06-04 17:18:01 +0300

    u

--- php/pagesMaking/tablePagesMaker.php
+++ php/pagesMaking/tablePagesMaker.php
@@ -7,10 +7,10 @@
 
         // $pageName = "history12";
         // $pageName = "history24";
-        $pageName = "history36";
+        // $pageName = "history36";
         // $pageName = "winners";
         // $pageName = "champ_league_current";
-        // $pageName = "euroleague_current";
+        $pageName = "euroleague_current";
         // $pageName = "decade";
         // $pageName = "5years";
 
